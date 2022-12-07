5e303658f16f72221b7fbbfce71f6a46e0d74695
samuelconlon <conlonsp@yahoo.com>
2022-12-06 20:13:26 -0500

    commit tree f7ff92b9599a7960755ded984e12e42a2d1d2626

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr)
+  arr.map do |a|
+    "Hello, my name is #{a}."
+  end
+end
+
+def assign_rooms(arr)
+  arr.map.with_index do |a, i|
+    "Hello, #{a}! You'll be assigned to room #{i += 1}!"
+  end
+end
+
+def printer(arr)
+  badges = batch_badge_creator(arr)
+  badges.each do |r|
+    puts "#{r}"
+  end
+  rooms = assign_rooms(arr)
+  rooms.each do |r|
+    puts "#{r}"
+  end
+end
